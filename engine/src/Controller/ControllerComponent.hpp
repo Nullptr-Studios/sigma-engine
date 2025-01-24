629b6fc2115a42e06c93de8b5f2ba2fb554334bd
Xein <xgonip@gmail.com>
2025-01-24 23:16:18 +0100

    fixed some things idk see diff

--- engine/src/Controller/ControllerComponent.hpp
+++ engine/src/Controller/ControllerComponent.hpp
@@ -7,18 +7,19 @@
  */
 #pragma once
 
-namespace FNFE {
-struct Transform;
-}
 struct AEVec3;
-namespace FNFE {
 
+namespace FNFE {
+  
+struct Transform;
+class Character;
+  
 /**
  * @class ControllerComponent Abstract Controller class for PlayerController and AIController to implement
  */
 class ControllerComponent {
 public:
-  ControllerComponent(Transform* transform) : m_transform(transform) {};
+  ControllerComponent(Character* character) : m_character(character) {};
   virtual ~ControllerComponent() = default;
   ControllerComponent(const ControllerComponent &) = delete;
   ControllerComponent &operator=(const ControllerComponent &) = delete;
@@ -27,8 +28,8 @@ public:
 
   
 protected:
-  AEVec3 m_position{}; ///<@brief 3D position of the object
-  Transform* m_transform{}; ///<@brief Pointer to the transform of the object
-  virtual void Update()=0; ///<@brief update function to update the player/ai
+  AEVec3 m_position{};              ///< @brief 3D position of the object
+  Character* m_character = nullptr; ///< @brief Pointer to the transform of the object
+  virtual void Update()=0;          ///< @brief update function to update the player/AI
 };
 } // namespace FNFE
