44b59f4bd8e2c98b29629a2a6231987b2e4ea28a
Xein <xgonip@gmail.com>
2025-01-29 11:46:57 +0100

    Created FromAEX tool

--- dev/src/TestScene.cpp
+++ dev/src/TestScene.cpp
@@ -7,26 +7,18 @@
  */
 
 #include "TestScene.hpp"
-#include "GlmAlphaTools.hpp"
-#include "aecore/AEEngine.h"
 #include <iostream>
 
 void TestScene::Load() {
   Scene::Load();
   std::cout << "TestScene::Load()" << std::endl;
 
-  glm::mat4 glmMatrix = {1, 2, 3, 4, 5, 6, 7,8, 9, 10, 11, 12, 13, 14, 15, 16};
-  AEMtx44 alphaMatrix = {1, 2, 3, 4, 5, 6, 7,8, 9, 10, 11, 12, 13, 14, 15, 16};
-
-  glm::ToAEX(glmMatrix).Print();
-  alphaMatrix.Print();
-  
 }
 void TestScene::Update(double delta) {
   Scene::Update(delta);
-  std::cout << "TestScene::Update(" << delta << ")" << std::endl;
+  // std::cout << "TestScene::Update(" << delta << ")" << std::endl;
 }
 void TestScene::Draw() {
   Scene::Draw();
-  std::cout << "TestScene::Draw()" << std::endl;
+  // std::cout << "TestScene::Draw()" << std::endl;
 }
--- engine/src/GlmAlphaTools.hpp
+++ engine/src/GlmAlphaTools.hpp
@@ -41,5 +41,39 @@ inline AEVec3 ToAEX(vec3& v) {
   return AEVec3 {v.x, v.y, v.z};
 }
 
+inline glm::vec2 FromAEX(AEVec2& v) {
+  return glm::vec2 {v.x, v.y};
+}
+
+inline glm::vec3 FromAEX(AEVec3& v) {
+  return glm::vec3 {v.x, v.y, v.z};
+}
+
+inline glm::mat3 FromAEX(AEMtx33& m) {
+  AEVec2 col1, col2, col3, row3;
+  // TF is this function @thomas -x
+  AEMtx33GetCol(&col1, 0, &m);
+  AEMtx33GetCol(&col2, 1, &m);
+  AEMtx33GetCol(&col3, 2, &m);
+  AEMtx33GetRow(&row3, 2, &m);
+
+  // FUCKING ALPHA ENGINE -x
+  // Just gamble and see if the last element is 1 or not -x
+  
+  return glm::mat3 {
+    col1.x, col2.x, col3.x,
+    col1.y, col2.y, col3.y,
+    row3.x, row3.y, 1.0f
+  };
+}
+
+inline glm::mat4 FromAEX(AEMtx44& m) {
+  return glm::mat4 {
+    m.RowCol(0,0), m.RowCol(0,1), m.RowCol(0,2), m.RowCol(0,3),
+    m.RowCol(1,0), m.RowCol(1,1), m.RowCol(1,2), m.RowCol(1, 3),
+    m.RowCol(2, 0), m.RowCol(2,1), m.RowCol(2,2), m.RowCol(2,3),
+    m.RowCol(3, 0), m.RowCol(3,1), m.RowCol(3,2), m.RowCol(3, 3)
+  };
+}
 
 }
