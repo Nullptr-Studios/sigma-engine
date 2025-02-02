3063af27fb2ef4139c2ce4d5f6c9c2595d1f7a9a
Xein <xgonip@gmail.com>
2025-02-02 17:34:55 +0100

    Added DebugDraw

--- engine/src/Collision/Collider.cpp
+++ engine/src/Collision/Collider.cpp
@@ -1,10 +1,14 @@
 #include "Collider.hpp"
 #include <Objects/Actor.hpp>
+#include "aecore/AEGraphics.h"
 
 namespace Sigma::Collision {
 
+// I think this should work but it's untested -x
+// Thanks alpha for having AEGfxRectMinMax -x
 void BoxCollider::DebugDraw(Actor* parent, unsigned color = AE_COLORS_GREEN) const {
-  // TODO: Do the debug draw
+  auto points = box.GetSides(parent->transform.position);
+  AEGfxRectMinMax(points[0], points[2], points[1], points[3], 0.0f, color); 
 }
 
 } // namespace Sigma
