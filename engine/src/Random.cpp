6b3a6b6d0d34e8a098b0918208a1008216e4b0f1
Dante Harper <dantedigdog@gmail.com>
2025-02-26 00:12:32 +0100

    started on random utils

---
+++ engine/src/Random.cpp
@@ -0,0 +1,23 @@
+#include <glm/vec2.hpp>
+#include "Random.hpp"
+
+namespace Sigma {
+
+int Random::Int(int min, int max) {
+  int spread = max-min;
+  return (std::rand()%spread) + min;
+  
+}
+float Random::Float(float min, float max) {
+  float spread = max-min;
+  return (std::rand()/ RAND_MAX) * spread + min;
+}
+
+glm::vec2 Random::Circle(float magnitude, float radius) {
+  float angle = Random::Float(0,360);
+  return {cos(angle)*magnitude,sin(angle)*magnitude};
+}
+
+
+}
+
---
+++ engine/src/Random.hpp
@@ -0,0 +1,36 @@
+/**
+ * @file Random.hpp
+ * @author Dante Harper
+ * @date 25/02/25
+ *
+ * @brief Provides Random Utilitys
+ */
+#pragma once
+#include <glm/vec2.hpp>
+namespace Sigma::Random {
+
+/**
+ * @brief
+ *
+ * @param max [TODO:parameter]
+ * @param min [TODO:parameter]
+ * @return [TODO:return]
+ */
+int Int(int min, int max);
+/**
+ * @brief 
+ *
+ * @param max [TODO:parameter]
+ * @param min [TODO:parameter]
+ * @return [TODO:return]
+ */
+float Float(float min, float max);
+/**
+ * @brief [TODO:description]
+ *
+ * @param radius [TODO:parameter]
+ */
+glm::vec2 Circle(float magnitude, float radius);
+
+}
+
